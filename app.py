86ce9c98856bc308522a56d1caf1b85b9589254d
isabella-34 <isamolial34@gmail.com>
2026-03-19 11:09:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
+st.subheader("¿Cómo estás? /n Haz gestos con la mano para indicar tu estado.")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
-st.image(image, width=350)
+#image = Image.open('OIG5.jpg')
+#st.image(image, width=350)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
 img_file_buffer = st.camera_input("Toma una Foto")
