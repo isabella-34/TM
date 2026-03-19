c2e1bfbe30ce42260eda18ed3425949cb3fd383f
isabella-34 <isamolial34@gmail.com>
2026-03-19 11:06:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,11 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
+      st.header('Bien, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+      st.header('Mal, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][2]>0.5:
+      st.header('Meh, con Probabilidad: '+str( prediction[0][2]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
